38c334f60d6bb3e2c7c82f2744fe832b1ce46dc6
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2011-02-25 14:00:00 +0100

    adding daily code-war

--- willThereBeEnoughSpace.js
+++ willThereBeEnoughSpace.js
@@ -18,6 +18,6 @@ cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting */
 
 
 function enough(cap, on, wait) {
-    return (on+wait > cap) ? on+wait-cap : 0
-       
-     }ss
+    return (on + wait > cap) ? on + wait - cap : 0
+
+}
