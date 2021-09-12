45fe580736d84dfdcafe4ec4a889c5bad62199e0
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-09-11 20:34:19 -0400

    codewars 1-5

---
+++ booleanToString.js
@@ -0,0 +1,8 @@
+/*Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+https://www.codewars.com/kata/551b4501ac0447318f0009cd */
+
+function booleanToString(b){
+    return b.toString()
+  }
