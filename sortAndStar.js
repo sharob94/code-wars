1fc1f4bf3ebbe1922f1aabee99e77aa00b2f91a5
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-10-16 21:22:46 -0400

    Adding daily code-wars

---
+++ sortAndStar.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/javascript
+
+/* you will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+
+given: function twoSort(s) {
+
+}
+
+
+
+
+*/
+
+
+function twoSort(s) {
+  
+    return s.sort()[0].split("").join("***")
+  
+  }
