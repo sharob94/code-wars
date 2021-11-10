05c1c6c35483e7eea85559bb598b042663b38675
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-11-09 20:49:50 -0500

    adding daily-codewars

---
+++ BeginnerLostWithoutAMap.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+*/
+
+
+function maps(x){
+    return x.map(x=> x * 2)
+  
+  }
