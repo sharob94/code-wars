c9aba72276691a5d0f6670530110362a0e5668ff
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-05 12:24:47 -0500

    adding daily code-wars

---
+++ function3multiplyingTwoNumbers.js
@@ -0,0 +1,16 @@
+/*
+
+https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+Implement a function which multiplies two numbers.
+
+
+
+*/
+
+
+
+
+function multiply(num1,num2){
+    return  num1 * num2
+    
+  }
