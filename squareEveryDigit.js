a1ccfeed13c368a9f9656816be9099b67bd588bf
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-09 12:50:49 -0500

    adding

---
+++ squareEveryDigit.js
@@ -0,0 +1,12 @@
+function squareDigits(num){
+    let answer = []
+    let separate = num.toString().split('')
+    for(let i = 0; i<separate.length; i++){
+      let parse = parseInt(separate[i])
+      
+      let n = Math.pow(parse,2)
+      
+      answer.push(n)
+    }
+    return parseInt(answer.join(''))
+  }
