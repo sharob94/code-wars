d1cfa4a45f081359df5c1ddace23bad84f4d1a04
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-16 12:45:50 -0500

    adding daily code-wars

---
+++ convertTheScore.js
@@ -0,0 +1,18 @@
+function scoreboard(string) {
+    let obj = {
+      'one': 1, 'two': 2, 'three' : 3, 'four' : 4,
+      'five' : 5, 'six' : 6, 'seven' : 7, 'eight' : 8,
+      'nine': 9, 'nil': 0
+    }
+    
+    let arr = []
+    let words = string.split(" ")
+    for (let i = 0; i < words.length; i++){
+      for (let voice in obj){
+        if(words[i].includes(voice)) {
+          arr.push(obj[voice])
+      }  
+    }
+  }
+    return arr
+  }
