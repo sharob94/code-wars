960af079088e8911b96389eeea2dd4093388d5e9
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-19 12:32:54 -0500

    adding daily code-wars

---
+++ differenceBetweenTwoCollections.js
@@ -0,0 +1,40 @@
+/*
+https://www.codewars.com/kata/594093784aafb857f0000122/train/javascript
+
+
+Find the difference between two collections. The difference means that either the character is present in one collection or it is present in other, but not in both. Return a sorted list with the difference.
+
+The collections can contain any character and can contain duplicates.
+
+Example
+A = [a, a, t, e, f, i, j]
+
+B = [t, g, g, i, k, f]
+
+difference = [a, e, g, j, k]
+
+
+
+
+*/
+
+
+function diff(a, b){
+
+    // filters out param A to only values that are not included in param B
+    const sortedA = a.filter(n=>!b.includes(n))
+    // filter out param B to only show vaues that are not included in param A
+    const sortedB = b.filter(n=>!a.includes(n))
+    // returns a new array with no duplicates and the values from sortedA combined with sorted B sorted
+    return [...new Set(sortedA.concat(sortedB))].sort()
+    }
+
+    /*
+    function diff(a,b){
+        sortedA = a.filter(n=>!b.includes(n))  
+        sortedB =
+    }
+    
+    
+    
+    */
