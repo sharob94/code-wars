6893490b28c1d33a92bb28fa8e9a0ad871b7d955
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-23 12:30:18 -0500

    adding daily code-wars

---
+++ practice.js
@@ -0,0 +1,34 @@
+// let familyMembers = [
+//     {
+//         name:'Jude', Relationship:'brother'},
+//     {name:'Rachel', Relationship:'sister'},
+// ]
+
+// console.log(familyMembers)
+
+let user = {
+    name: 'shana' ,
+    age : 27,
+    location: 'philly',
+    occupation: 'software engineer',
+    duties:[{
+        name:'Jude', Relationship:'brother'},
+    {name:'Rachel', Relationship:'sister'}],
+    login: function(){
+        console.log(this.duties)
+    },
+    logout: function(){
+        console.log('I logged out')
+    },
+    logduties:function(){
+        this.duties.forEach(duty=>{
+            console.log(duty.name,duty.Relationship)
+        })
+    }
+
+
+}
+user.login()
+user.logduties()
+
+ 
