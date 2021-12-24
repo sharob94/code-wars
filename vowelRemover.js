3588e10495734b6937886ffefa67a669a868b9b6
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-24 12:27:59 -0500

    adding daily code-wars

---
+++ vowelRemover.js
@@ -0,0 +1,20 @@
+/*
+https://www.codewars.com/kata/5547929140907378f9000039/train/javascript
+
+
+Create a function called shortcut to remove all the lowercase vowels in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+
+
+
+*/
+
+
+function shortcut(string){
+    return string.replace(/[aeiou]/g,'')
+  }
