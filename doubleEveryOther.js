f76dca03db547256b6026ad1ba9ed35ec2122c04
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-26 09:59:24 -0500

    adding daily code-wars

---
+++ doubleEveryOther.js
@@ -0,0 +1,19 @@
+
+/*
+
+Write a function that doubles every second integer in a list, starting from the left.
+
+Example:
+
+doubleEveryOther([1,2,3,4]) => [ 1, 4, 3, 8 ]
+
+
+*/
+
+function doubleEveryOther(a) {
+    console.log(a, 'arr')
+    for (let i = 1; i < a.length; i+=2){
+        a[i] *= 2
+      }
+    return a
+  }
