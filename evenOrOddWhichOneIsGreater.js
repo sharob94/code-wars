09161b2122a81a305161d928d44ee8a6f39c4af6
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2021-12-29 10:10:59 -0500

    adding daily code-wars

---
+++ evenOrOddWhichOneIsGreater.js
@@ -0,0 +1,11 @@
+function evenOrOdd(str) {
+    let evenSum = 0
+    let oddSum = 0
+    for(let i = 0; i < str.length; i++){
+      if(parseInt(str[i]) % 2 === 0) evenSum += parseInt(str[i])
+      else oddSum += parseInt(str[i])
+    }
+      if(evenSum > oddSum) return "Even is greater than Odd"
+      if(oddSum > evenSum) return "Odd is greater than Even"
+      if(oddSum == evenSum) return "Even and Odd are the same"
+    }
