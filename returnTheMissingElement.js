299eec7f3421a1626b7a1963eea7f912231f681e
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2022-01-01 10:14:12 -0500

    adding daily code-wars

---
+++ returnTheMissingElement.js
@@ -0,0 +1,10 @@
+function getMissingElement(superImportantArray){
+    let count = 0
+    let array = superImportantArray.sort((a,b) => a-b)
+     for(let i = 0 ; i < array.length; i++){
+       if(i===array[i]){
+         count++
+       }
+     }
+    return count
+  }
