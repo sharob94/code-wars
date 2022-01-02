27ee1300712e8886d4aa2d477c38380159084624
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2022-01-02 10:15:00 -0500

    adding daily code-wars

---
+++ spongebobmeme.js
@@ -0,0 +1,10 @@
+function spongeMeme(sentence) {
+    sentence = sentence.toLowerCase()
+    let answer = ""
+    for(let i = 0; i < sentence.length; i++){
+      if(i % 2 === 0) answer += sentence[i].toUpperCase()
+      else answer += sentence[i].toLowerCase()
+    }
+    console.log(answer)
+    return answer
+  }
