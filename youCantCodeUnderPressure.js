defbc0ec5fc8cfbc5a8a5d3c27d00f637683cd9e
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2022-02-25 10:17:45 -0500

    adding daily codeWar

--- youCantCodeUnderPressure.js
+++ youCantCodeUnderPressure.js
@@ -8,7 +8,7 @@ Code as fast as you can! You need to double the integer and return it.
 */
 
 function doubleInteger(i) {
-  
-    // i will be an integer. Double it and return it.
-    return i * 2;
-  }ss
+
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
