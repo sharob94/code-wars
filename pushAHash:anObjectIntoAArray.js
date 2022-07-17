f8b9ce869b8e7bdcdab5d4a57aa39dffe4250adf
Shana Robinson <88958905+sharob94@users.noreply.github.com>
2022-07-16 20:19:19 -0500

    adding daily codeWar

---
+++ pushAHash:anObjectIntoAArray.js
@@ -0,0 +1,14 @@
+/*
+You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+
+
+
+*/
+
+let items = []
+let obj = {
+    a: "b",
+    c: "d"
+}
+items.push(obj)
---
+++ sumWithoutHighestandLowestNumber.js
@@ -0,0 +1,42 @@
+/*
+Task
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+
+
+
+
+*/
+
+
+function sumArray(array) {
+    if (array === null || array === [] || array === undefined || array.length <= 2) {
+        return 0
+    } else {
+        let sortedArray = array.sort((a, b) => a - b)
+        sortedArray.pop()
+        sortedArray.shift()
+        let sum = sortedArray.reduce((a, c) => a + c)
+        return sum
+
+    }
+
+}
+
+
+  // first take care of the edge cases where array length is less than 1
+  // next sort array 
+  // next array. pop to remove last index
+  // array. shift to remove first index
+  // reduce to get some 
+  // return sum
